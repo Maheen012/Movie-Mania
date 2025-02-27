39af77bcdd9eeabbff896956611f2affb05caa12
Matthew delaney <matt.delaney404@gmail.com>
2025-02-27 16:26:39 -0500

    This should work, but idk

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends Application {
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        primaryStage = stage;
+        switchScene("LoginScreen.fxml");
+        primaryStage.setTitle("Movie Mania");
+        primaryStage.show();
+    }
+
+    public static void switchScene(String fxmlFile) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        primaryStage.setScene(scene);
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
